5f4cf05bd85ca2311819d2fce6a9e584f64487f2
Ariannebez <72392619+Ariannebez@users.noreply.github.com>
2023-11-19 22:40:01 +0100

    AboutUs page content, adding img of artist & info.

--- aboutUs.php
+++ aboutUs.php
@@ -11,24 +11,30 @@
     <title>About Us</title>
 </head>
 <body>
-<div class="container">
-    <div class="row">
+<div class="container-fluid text-center">
+    <div class="row p-5">
         <!-- 2 cols in 1 row -->
         <!--Img col -->
-        <div class="col">
-        <img src="https://media.istockphoto.com/id/1211291912/photo/girl-is-making-new-beaded-jevellery.jpg?s=612x612&w=0&k=20&c=1hIWn9HpGSBrlpbJ_MF_-QPBzC3iB9AcysYURE6zod4=" alt="Women making art">
+        <div class="col align-self-center" >
+        <img class="rounded float-start" src="https://media.istockphoto.com/id/1211291912/photo/girl-is-making-new-beaded-jevellery.jpg?s=612x612&w=0&k=20&c=1hIWn9HpGSBrlpbJ_MF_-QPBzC3iB9AcysYURE6zod4=" alt="Women making art">
         </div>
 
         <!--Text area col-->
-        <div class="col">
+        <div class="col align-self-center">
+            <p>
 
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
+                dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                 It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
+                 with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+            </p>
         </div>
     </div>
 </div>
 
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 </html>
