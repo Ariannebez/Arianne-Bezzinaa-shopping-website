2823a4e4f1040afe5dcdd6690ce2fdbcb09503df
Ariannebez <72392619+Ariannebez@users.noreply.github.com>
2023-11-23 08:50:49 +0100

    user management table

---
+++ Admin/user-management.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+        <div class="col">Id</div>
+
+        <div class="col">Name</div>
+
+        <div class="col">Surname</div>
+
+        <div class="col">Role</div>
+
+        <div class="col">Varififed</div>
+
+        <div class="col">Password</div>
+
+        <div class="col">Contact Number</div>
+
+        <div class="col">Actions</div>
+
+        <!--row closing-->
+        </div>
+
+        <div class="row">
+        <div class="col">Id</div>
+
+        <div class="col">Ari</div>
+
+        <div class="col">Bezz</div>
+
+        <div class="col">Admin</div>
+
+        <div class="col">Yes</div>
+
+        <div class="col"> </div>
+
+        <div class="col">343453454</div>
+
+        <div class="col">Edit/Delet</div>
+
+        </div>
+    </div>
+    
+</body>
+</html>
