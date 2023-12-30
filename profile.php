7f5988f4d5be06175b2e340f725b6b6cd16efc5e
Ariannebez <72392619+Ariannebez@users.noreply.github.com>
2023-12-30 15:32:00 +0100

    fixing errors

--- profile.php
+++ profile.php
@@ -3,97 +3,41 @@
         require_once "includes/functions.php";
         require_once "includes/dbh.php";
         require_once "includes/db-functions.php";
-    ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="includes/style.css">
-</head>
-<body>
- 
 
-<div class="container">
+session_start();
 
-<div class="row mt-5">
-<div class="row text-center"><h1>My Account</h1></div>
+if (!isset($_SESSION['username'])) {
+    // Redirect to login page
+    header("Location: login.php");
+    exit;
+}
 
-<div class="card">
-<h5 class="card-header">My Profile</h5>
-<div class="card-body">
-<h5 class="card-title">
-<a class="btn btn-primary me-3" href="">
-<svg xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
-</a>
-    Arianne Bezzina</h5>
-    <p>Click to edit</p>
-
-</div>
-</div>
-</div>
-<!--row closes -->
-</div>
-<!--con closes -->
+// Database connection
+$conn = new mysqli("hostname", "username", "password", "database");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 
-<!-- new con-->
-<div class="container">
-
-<div class="row mt-5">
-
-
-<div class="card">
-<h5 class="card-header">My Orders</h5>
-<div class="card-body">
-<h5 class="card-title">
-<a class="btn btn-primary me-3" href="">
-<svg xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
-</a>
-Review Orders</h5>
-<p>Click to View</p>
-</div>
-</div>
-</div>
-<!--row closes -->
-</div>
-<!--con closes -->
+$username = $_SESSION['username'];
+$stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
 
 
-<!-- new con-->
-<div class="container">
+if ($user) {
+    echo "Welcome " . htmlspecialchars($user['name']) . "<br>";
+    echo "Email: " . htmlspecialchars($user['email']) . "<br>";
+    // other details
+} else {
+    echo "User not found";
+}
 
-<div class="row mt-5 mb-5">
-
-
-<div class="card ">
-<h5 class="card-header">Returns</h5>
-<div class="card-body">
-<h5 class="card-title">
-<a class="btn btn-primary me-3" href="">
-<svg xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M32 96l320 0V32c0-12.9 7.8-24.6 19.8-29.6s25.7-2.2 34.9 6.9l96 96c6 6 9.4 14.1 9.4 22.6s-3.4 16.6-9.4 22.6l-96 96c-9.2 9.2-22.9 11.9-34.9 6.9s-19.8-16.6-19.8-29.6V160L32 160c-17.7 0-32-14.3-32-32s14.3-32 32-32zM480 352c17.7 0 32 14.3 32 32s-14.3 32-32 32H160v64c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-96-96c-6-6-9.4-14.1-9.4-22.6s3.4-16.6 9.4-22.6l96-96c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 64H480z"/></svg>
-</a>
-Return orders</h5>
-<p>Click to View</p>
-</div>
-</div>
-</div>
-<!--row closes -->
-</div>
-<!--con closes -->
+$conn->close();
 
 
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-</body>
-</html>
 
-<?php
-        include 'includes/footer.php';
-    ?>
+        include 'includes/footer.php'?>
